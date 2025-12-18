79ca8a7bdfe2795bf48cb3850f1f1e12c047d016
tien-boop <thuy.tien.tran@edu.huddinge.se>
2025-12-18 10:21:03 +0000

    done

--- Proccesed/flagga.py
+++ Proccesed/flagga.py
@@ -1,17 +1,23 @@
-from PIL import Image, ImageDraw 
+from PIL import Image, ImageDraw, ImageOps
 
 red=(218,37,29)
 yellow = (255,255,0)
 blue = 	(51, 153, 255)
 
-star = Image.open("Images/star.png")
-
 im = Image.new("RGB",(160,100),red)
 
 imd = ImageDraw.Draw(im)
 
-imd.rectangle([(0,50),(160,50)],blue)
-imd.rectangle([(50, 0), (70, 100)], blue)
+imd.rectangle([(0,50),(160,100)],blue)
+
+star = Image.open("Images/star.png").convert("RGBA")
+
+star = ImageOps.scale(star,0.05)
+
+
+im.paste(star, (53, 20), star)
+im.show()
+
 
 
 im.save("flag.png")
